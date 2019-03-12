e7bff2162eeba60f950e78fa777cfb2f6ba32fbe
tinalim <lst_tina@163.com>
2019-03-12 10:20:21 +0800

    浏览器前缀配置

--- server-setting/build/main.js
+++ server-setting/build/main.js
@@ -97,14 +97,14 @@ eval("exports = module.exports = __webpack_require__(/*! ../node_modules/css-loa
 
 /***/ }),
 
-/***/ "./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/lib/loader.js!./public/index.scss":
-/*!**********************************************************************************************************!*\
-  !*** ./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/lib/loader.js!./public/index.scss ***!
-  \**********************************************************************************************************/
+/***/ "./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/lib/loader.js!./node_modules/postcss-loader/src/index.js?!./public/index.scss":
+/*!******************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/lib/loader.js!./node_modules/postcss-loader/src??ref--6-3!./public/index.scss ***!
+  \******************************************************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("exports = module.exports = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\")(false);\n// Module\nexports.push([module.i, \"#header {\\n  color: #55aaee; }\\n\", \"\"]);\n\n\n\n//# sourceURL=webpack:///./public/index.scss?./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/lib/loader.js");
+eval("exports = module.exports = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ \"./node_modules/css-loader/dist/runtime/api.js\")(false);\n// Module\nexports.push([module.i, \"#header {\\n  display: -webkit-box;\\n  display: -webkit-flex;\\n  display: -moz-box;\\n  display: -ms-flexbox;\\n  display: flex;\\n  color: #55aaee; }\\n\", \"\"]);\n\n\n\n//# sourceURL=webpack:///./public/index.scss?./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/lib/loader.js!./node_modules/postcss-loader/src??ref--6-3");
 
 /***/ }),
 
@@ -194,7 +194,7 @@ eval("\nvar content = __webpack_require__(/*! !../node_modules/css-loader/dist/c
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("\nvar content = __webpack_require__(/*! !../node_modules/css-loader/dist/cjs.js!../node_modules/sass-loader/lib/loader.js!./index.scss */ \"./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/lib/loader.js!./public/index.scss\");\n\nif(typeof content === 'string') content = [[module.i, content, '']];\n\nvar transform;\nvar insertInto;\n\n\n\nvar options = {\"hmr\":true}\n\noptions.transform = transform\noptions.insertInto = undefined;\n\nvar update = __webpack_require__(/*! ../node_modules/style-loader/lib/addStyles.js */ \"./node_modules/style-loader/lib/addStyles.js\")(content, options);\n\nif(content.locals) module.exports = content.locals;\n\nif(false) {}\n\n//# sourceURL=webpack:///./public/index.scss?");
+eval("\nvar content = __webpack_require__(/*! !../node_modules/css-loader/dist/cjs.js!../node_modules/sass-loader/lib/loader.js!../node_modules/postcss-loader/src??ref--6-3!./index.scss */ \"./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/lib/loader.js!./node_modules/postcss-loader/src/index.js?!./public/index.scss\");\n\nif(typeof content === 'string') content = [[module.i, content, '']];\n\nvar transform;\nvar insertInto;\n\n\n\nvar options = {\"hmr\":true}\n\noptions.transform = transform\noptions.insertInto = undefined;\n\nvar update = __webpack_require__(/*! ../node_modules/style-loader/lib/addStyles.js */ \"./node_modules/style-loader/lib/addStyles.js\")(content, options);\n\nif(content.locals) module.exports = content.locals;\n\nif(false) {}\n\n//# sourceURL=webpack:///./public/index.scss?");
 
 /***/ })
 
--- server-setting/webpack.config.js
+++ server-setting/webpack.config.js
@@ -6,15 +6,15 @@ module.exports = {
         filename: '[name].js',
         path: path.resolve(__dirname, 'build')
     },
-    devServer: {                // 配置 webpack-dev-server
+    devServer: { //====== 配置 webpack-dev-server
         contentBase: './build', // 设置服务器访问的基本路径
         host: 'localhost',      // 服务器ip地址
         port: 8080,             // 端口
         open: true              // 自动打开页面
     },
     module: {
-        rules: [                     // loader配置
-            {
+        rules: [ //====== loader配置
+            {       //====== 配置编译css文件
                 test: /\.css$/,      // 用于标识应该被相应loader进行转换的某个或某些文件
                 use: [               // 表示进行转换时，应该使用哪个loader
                     'style-loader',  // style-loader的顺序必须之于css-loader之上
@@ -22,7 +22,7 @@ module.exports = {
                     'css-loader'
                 ]
             },
-            {
+            {       //====== 配置编译less文件
                 test: /\.less$/,
                 use: [
                   'style-loader',
@@ -30,12 +30,20 @@ module.exports = {
                   'less-loader'      // 支持编译less(前提是安装less)
                 ]
             },
-            {
+            {       //====== 配置编译scss文件
                 test: /\.scss$/,
                 use: [
                     'style-loader',
                     'css-loader',
-                    'sass-loader'   // 支持编译sass文件(前提是安装node-sass)
+                    'sass-loader',   // 支持编译sass文件(前提是安装node-sass)
+                    { //====== 配置添加浏览器前缀
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: [
+                              require('autoprefixer') // (必须添加浏览器版本设置)浏览器版本设置在package.json的browserlist里
+                            ]
+                        }
+                    }
                 ]
             }
         ]
