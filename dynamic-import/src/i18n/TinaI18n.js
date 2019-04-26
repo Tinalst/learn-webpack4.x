98a66311b78b74908ee223425d56e4b4df171f3e
tinalim <lst_tina@163.com>
2019-04-26 19:17:57 +0800

    动态导入

--- dynamic-import/src/i18n/TinaI18n.js
+++ dynamic-import/src/i18n/TinaI18n.js
@@ -4,7 +4,8 @@ export class TinaI18n {
     this.useFileName = obj['useFileName'];
   }
 
-  checkLang(){
+  // 设置当前国际化语言
+  setLang(){
     if(/^zh/i.test(this.currentLang)){
       if(/^zh[-—](cn|SG)/gi.test(this.currentLang)){
         this.currentLang = 'zh-cn';
@@ -12,34 +13,56 @@ export class TinaI18n {
         this.currentLang = 'zh-hant';
       }
     }else {
-      this.currentLang = JsSrc.slice(0,2).toLowerCase();
+      this.currentLang = this.currentLang.slice(0,2).toLowerCase();
     }
   }
 
-  setInnerHtml(){
-    console.log(2)
+  // 获取当前国际化语言翻译文案
+  static getI18nFile(fileUrl){
+    return import(/* webpackIgnore: true */ fileUrl).then(({default: ctx}) => {
+      return ctx;
+    })
+  }
+
+  // 变换国际化
+  setInnerHtml(lang){
+    lang && (this.currentLang = lang);
     let ele = document.querySelectorAll('.i18n');
     if (!(ele.length > 0)) {
       return new Error('请检查class是否有添加i18n');
     }
-    this.checkLang();
-    console.log(`./${this.currentLang}/${this.useFileName}.js`);
-
-    // let url = `./${this.currentLang}/${this.useFileName}.js`;
-    // for (let i = 0; i < ele.length; i++) {
-    //   if (this.currentLang && pack[this._type][this._lang]) {
-    //     if (!ele[i].getAttribute('placeholder')) {
-    //       ele[i].innerText = pack[this._type][this._lang][ele[i].getAttribute('data-i18n')];
-    //     }
-    //   } else {
-    //     if (!(pack[this._type])) {
-    //       console.error('未指定' + this._lang + '文件国际化js文件未配置');
-    //       return
-    //     } else if (!(pack[this._type][this._lang])) {
-    //       console.error('指定的' + this._lang + '语言js文件未引入');
-    //       return
-    //     }
-    //   }
+    this.setLang();
+    // switch (this.currentLang) {
+    //   case 'zh-cn':
+    //     import(/* webpackIgnore: true */ `./i18n/${this.currentLang}/${this.useFileName}.js`).then(({default: ctx}) => {
+    //       console.log('ctx-1', ctx);
+    //     });
+    //     break;
+    //   case 'zh-hant':
+    //
+    //     // import('./zh-hant/addWallet.js').then(({default: ctx}) => {
+    //     //   console.log('ctx', ctx);
+    //     // });
+    //     break;
     // }
+    console.log(`./i18n/${this.currentLang}/${this.useFileName}.js`);
+    // TinaI18n.getI18nFile(`./i18n/${this.currentLang}/${this.useFileName}.js`).then(ctx => {
+    // TinaI18n.getI18nFile(`http://127.0.0.1:8080/dist/i18n/${this.currentLang}/${this.useFileName}.js`).then(ctx => {
+    TinaI18n.getI18nFile(`http://127.0.0.1:33333/i18n/${this.currentLang}/${this.useFileName}.js`).then(ctx => {
+      console.log('ctx-0', ctx);
+      for (let i = 0; i < ele.length; i++) {
+        if (this.currentLang && ctx) {
+          if (!ele[i].getAttribute('placeholder')) {
+            ele[i].innerText = ctx[ele[i].getAttribute('data-i18n')];
+          }
+        } else {
+          if (!(ctx)) {
+            console.error('获取文案失败： 路径指定错误？ 文件不存在？');
+            return
+          }
+        }
+      }
+    });
   }
+
 }
--- dynamic-import/src/index.js
+++ dynamic-import/src/index.js
@@ -1,3 +1,27 @@
-import {mins} from "./math";
+import {TinaI18n} from "./i18n/TinaI18n";
 
-console.log(add(1, 2));
+window.onload = function () {
+  // 实例化国际化
+  const tinaI18n = initI18N('zh-hant', 'addWallet');
+
+  // 简体中文
+  document.querySelector('#cn').addEventListener('click', ()=>{
+    tinaI18n.setInnerHtml('zh-cn');
+  });
+
+  // 繁体中文
+  document.querySelector('#hant').addEventListener('click', ()=>{
+    tinaI18n.setInnerHtml('zh-hant');
+  })
+};
+
+// 初始化国际化
+function initI18N(currentlang, currentFile) {
+  const tinaI18n =  new TinaI18n({
+    currentLang: currentlang,
+    useFileName: currentFile
+  });
+  tinaI18n.setInnerHtml();
+  return tinaI18n;
+}
+
