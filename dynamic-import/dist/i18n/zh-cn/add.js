abc2227c6acf7af40ec5acb76e9772e3dd15a4bc
tinalim <lst_tina@163.com>
2019-05-20 18:10:03 +0800

    [test] pdx-i18n

---
+++ dynamic-import/dist/i18n/zh-cn/add.js
@@ -0,0 +1,6 @@
+export default {
+  "btnCreate": "创建",
+  "btnImport": "导入",
+  "placeholder": "名字",
+  "i18nObj": {"title": "翻译变量"}
+}
---
+++ dynamic-import/dist/i18n/zh-hant/add.js
@@ -0,0 +1,6 @@
+export default {
+  "btnCreate": "創建",
+  "btnImport": "導入",
+  "placeholder": "名字（繁）",
+  "i18nObj": {"title": "翻译变量（繁）"}
+}
